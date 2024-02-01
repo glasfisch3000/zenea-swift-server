3e9ecbc39e9b4240196db01653c1db61a382c79a
glasfisch3000 <jakob@danckwerts.net>
2024-02-01 14:51:56 +0100

    updated OS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "zenea-swift-server",
     platforms: [
-       .macOS(.v13)
+       .macOS("13.3")
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.92.1"),
